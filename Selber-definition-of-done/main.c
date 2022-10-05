3612955ca9d3beb02375283ee56658577a7f3068
aabishtkhh <aabish.tariqkhan@noseryoung.com>
2022-10-05 15:31:54 +0200

    Selbst Dod

---
+++ Selber-definition-of-done/main.c
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Aabish Tariq
+ *
+ * Created on 5. Oktober 2022, 14:29
+ */
+
+#include <stdio.h>
+#include <stdib.h>
+#include "Aufgabe.h"
+
+/*
+ * 
+ */
+int main {
+
+    float a;
+    float b;
+    float result
+
+    printf("Multiplikation Rechnen: \n");
+
+    printf("\n-------------------------- \n");
+
+    printf("\nGib die erste Zahl ein: ");
+    scanf("%d", a);
+
+    printf("\nGib die zweite Zahl ein: ");
+    scanf("%d", b);
+
+    printf("\n-------------------------- \n");
+
+    printf("Die Resultat ist: %d", (a*b)):
+
+
+    return;
+}
+
